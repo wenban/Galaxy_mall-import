23d6df5d71b670289771af93de08f93c1cd2dfef
jamesLFrank08 <jamesLFrank08@8f565724-67c7-4889-beb6-a003d35922bc>
2016-06-04 12:27:47 +0000

    更新添加sql相应的DAO 						--JamesLFrank

--- trunk/src/main/java/galaxy/dao/UserDAO.java
+++ trunk/src/main/java/galaxy/dao/UserDAO.java
@@ -10,16 +10,28 @@ import galaxy.model.User_history;
 
 @Repository
 public interface UserDAO {
+	
 	public List<User> selectUser(User user);
+
 	public Integer insertIntoUser(User user);
+
 	public User selectUserToLogin(String loginId);
+
 	public List<User_history> selectGoodsByHistory(User user);
+
 	public int deleteHistoryById(User user);
+
 	public User getUserInfo(User user);
+
 	public int updateUserInfo(User user);
+
 	public int updateUserAddInfo(UserAddress userAddress);
+
 	public List<UserAddress> getUserAddrByUserId(User user);
+
 	public int insertIntoUserAddr(UserAddress userAddress);
+
 	public int updateSrcIntoUser(User user);
-	public int setStoreId(User user);
+
+	public int setStoreIdToUserByUserId(User user);
 }
