222e4bd87910e702c68029484e420c355812049c
Amanda2016 <Amanda2016@8f565724-67c7-4889-beb6-a003d35922bc>
2016-06-05 02:21:07 +0000

    多了一个删除用户地址的功能

--- trunk/src/main/java/galaxy/service/UserService.java
+++ trunk/src/main/java/galaxy/service/UserService.java
@@ -198,6 +198,10 @@ public class UserService {
 		return imagesDomain+newName;
 	}
 
+	public int deleteAddrInfo(UserAddress userAddress) {
+		return userDAO.deleteUserAddrById(userAddress);
+	}
+
 	
 
 }
